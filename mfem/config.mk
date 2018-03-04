51ea941a39d48f1c8fd13fc608cdb27403f094e4
Alexey Khomenko <hom.ksei@gmail.com>
2018-03-04 11:45:18 +0300

    first commit

---
+++ mfem/config.mk
@@ -0,0 +1,48 @@
+# Copyright (c) 2010, Lawrence Livermore National Security, LLC. Produced at the
+# Lawrence Livermore National Laboratory. LLNL-CODE-443211. All Rights reserved.
+# See file COPYRIGHT for details.
+#
+# This file is part of the MFEM library. For more information and source code
+# availability see http://mfem.org.
+#
+# MFEM is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License (as published by the Free
+# Software Foundation) version 2.1 dated February 1999.
+
+# Variables corresponding to defines in config.hpp (YES, NO, or value)
+MFEM_USE_MPI         = NO
+MFEM_USE_METIS_5     = NO
+MFEM_DEBUG           = NO
+MFEM_USE_GZSTREAM    = NO
+MFEM_USE_LIBUNWIND   = NO
+MFEM_USE_LAPACK      = NO
+MFEM_THREAD_SAFE     = NO
+MFEM_USE_OPENMP      = NO
+MFEM_USE_MEMALLOC    = YES
+MFEM_TIMER_TYPE      = 0
+MFEM_USE_SUNDIALS    = NO
+MFEM_USE_MESQUITE    = NO
+MFEM_USE_SUITESPARSE = NO
+MFEM_USE_SUPERLU     = NO
+MFEM_USE_GECKO       = NO
+MFEM_USE_GNUTLS      = NO
+MFEM_USE_NETCDF      = NO
+MFEM_USE_PETSC       = NO
+MFEM_USE_MPFR        = NO
+MFEM_USE_SIDRE       = NO
+
+# Compiler, compile options, and link options
+MFEM_CXX       = g++
+MFEM_CPPFLAGS  =
+MFEM_CXXFLAGS  = -O3
+MFEM_TPLFLAGS  =
+MFEM_INCFLAGS  = -I/Users/Dr.Iohanson/Desktop/mfem-3.3/mfem/include
+MFEM_FLAGS     =  -O3 -I/Users/Dr.Iohanson/Desktop/mfem-3.3/mfem/include
+MFEM_LIBS      = -L/Users/Dr.Iohanson/Desktop/mfem-3.3/mfem/lib -lmfem
+MFEM_LIB_FILE  = /Users/Dr.Iohanson/Desktop/mfem-3.3/mfem/lib/libmfem.a
+MFEM_BUILD_TAG = Darwin MacBook-Pro-Alexey.local x86_64
+MFEM_PREFIX    = /Users/Dr.Iohanson/Desktop/mfem-3.3/mfem
+MFEM_INC_DIR   = /Users/Dr.Iohanson/Desktop/mfem-3.3/mfem/include
+MFEM_LIB_DIR   = /Users/Dr.Iohanson/Desktop/mfem-3.3/mfem/lib
+
+# Optional extra configuration
